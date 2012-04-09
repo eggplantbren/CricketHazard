9304f12d97dad474aa54c66ca3f875bb429fdb8c
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-09 10:09:46 -0700

    Added comments, turned things into numpy arrays, renamed Data class to Career

--- Data.py
+++ Data.py
@@ -1,29 +1,66 @@
-class Data:
+import numpy as np
+
+"""
+An object of this class is a single player's career.
+Just a chronological list of integer scores and 
+boolean flags for out (True)/not out (False).
+"""
+class Career:
+	"""
+	Constructor: just sets scores and outFlags to
+	empty arrays.
+	"""
 	def __init__(self):
 		self.clear()
 	
+	"""
+	Set scores and outFlags to empty arrays.
+	"""
 	def clear(self):
-		self.scores = []
-		self.outFlags = []
+		self.scores = np.array([])
+		self.outFlags = np.array([])
 
+	"""
+	Load from file. Parses * notation for not-outs.
+	"""
 	def load(self, filename):
+		scores = []
+		outFlags = []
 		f = open(filename, "r")
 		lines = f.readlines()
 		for line in lines:
 			line = line.strip()
-			print(line)
 			if len(line) > 0:
 				if line[-1] == "*":
-					self.outFlags.append(False)
+					outFlags.append(False)
 					line = line[0:(len(line)-1)]
 				else:
-					self.outFlags.append(True)
-				self.scores.append(int(line))
-		print self.scores
-		print self.outFlags
+					outFlags.append(True)
+				scores.append(int(line))
+		self.scores = np.array(scores)
+		self.outFlags = np.array(outFlags)
+
+	"""
+	Turn a single innings to a string.
+	"""
+	def toString(self, i):
+		result = `self.scores[i]`
+		if not self.outFlags[i]:
+			result += '*'
+		return result
+
+	"""
+	Convert a whole career to a string.
+	"""
+	def __str__(self):
+		return ''.join([self.toString(i) + ' ' for i in xrange(0, self.scores.size)])
 
 
+"""
+Simple main, for testing
+"""
 if __name__ == '__main__':
-	data = Data()
+	data = Career()
 	data.load("fake_data.txt")
+	print(data)
 
