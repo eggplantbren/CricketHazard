ed05fe3a34b751d317890e76585712ab71ab0c19
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-09 12:48:48 -0700

    Implemented Metropolis proposals

--- HazardModel.py
+++ HazardModel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.random as rng
 
+
 class HazardModel:
 	"""
 	An object of this class is a point in the parameter
@@ -9,6 +10,14 @@ class HazardModel:
 
 	# A static variable, the x-axis
 	x = np.arange(0, 500)
+
+	# Hyperparameters
+	logMuMin = np.log(1.0)
+	logMuMax = np.log(1E2)
+	logMuRange = logMuMax - logMuMin
+	logLMin = np.log(0.1)
+	logLMax = np.log(1E2)
+	logLRange = logLMax - logLMin
 
 	def __init__(self, mu0=40.0, mu1=40.0, L=5.0):
 		"""
@@ -23,11 +32,14 @@ class HazardModel:
 		Draw the parameters from the prior. Currently using
 		an ignorant prior.
 		"""
-		self.mu0 = np.exp(np.log(1.0) + np.log(1E2)*rng.rand())
-		self.mu1 = np.exp(np.log(1.0) + np.log(1E2)*rng.rand())
+		self.mu0 = np.exp(HazardModel.logMuMin\
+				 + HazardModel.logMuRange*rng.rand())
+		self.mu1 = np.exp(HazardModel.logMuMin\
+				 + HazardModel.logMuRange*rng.rand())
 		if self.mu1 < self.mu0:
 			self.mu0, self.mu1 = self.mu1, self.mu0
-		self.L = np.exp(np.log(0.1) + np.log(1E3)*rng.rand())
+		self.L = np.exp(HazardModel.logLMin\
+				 + HazardModel.logLRange*rng.rand())
 		self.compute()
 
 	def compute(self):
@@ -47,6 +59,67 @@ class HazardModel:
 		# Note: can be underestimated due to range of HazardModel.x!
 		self.average = np.sum(HazardModel.x*np.exp(self.logf))
 
+	def proposal(self, other=None):
+		"""
+		Proposal for Metropolis-Hastings updates.
+		Prior should be implicit in this.
+		Return value: log of a hastings factor.
+		Optional argument: another object, to do a stretch move
+		"""
+
+		logH = 0.0
+		if other == None:
+			# Standard Metropolis proposal
+			which = rng.randint(3)
+			if which==0:
+				param = self.mu0
+				lower = HazardModel.logMuMin
+				scale = HazardModel.logMuRange
+			elif which==1:
+				param = self.mu1
+				lower = HazardModel.logMuMin
+				scale = HazardModel.logMuRange
+			else:
+				param = self.L
+				lower = HazardModel.logLMin
+				scale = HazardModel.logLRange
+
+			param = np.log(param)
+			param += scale*10.0**(1.5 - 6.0*rng.rand())\
+					*rng.randn()
+			param = np.mod(param - lower, scale) + lower
+			param = np.exp(param)
+
+			if which==0:
+				self.mu0 = param
+			elif which==1:
+				self.mu1 = param
+			else:
+				self.L = param
+			
+				
+		else:
+			# Stretch move
+			print("Stretch move not implemented yet.")
+			
+
+		if self.mu1 < self.mu0:
+			self.mu0, self.mu1 = self.mu1, self.mu0
+		self.compute()
+		return logH
+
+	def logPrior(self):
+		""" Evaluate the prior density at the current point.
+		Stretch moves need this."""
+		if self.mu0 < HazardModel.logMuMin\
+		or self.mu0 > HazardModel.logMuMax\
+		or self.mu1 < HazardModel.logMuMin\
+		or self.mu1 > HazardModel.logMuMax\
+		or self.L   < HazardModel.logLMin \
+		or self.L   > HazardModel.logLMax:
+			return -np.inf
+		return 0.0
+
 	def __str__(self):
 		return str(self.mu0) + " " + str(self.mu1) + " " + str(self.L)
 		
@@ -59,18 +132,20 @@ if __name__ == '__main__':
 	import time
 
 	m = HazardModel()
+	m.fromPrior()
 
 	plt.ion()
 	plt.hold(False)
 	for i in xrange(0, 100):
-		m.fromPrior()
+		m.proposal()
+
 		plt.plot(HazardModel.x, m.mu)
 		plt.title("Prior over a player's effective average curve")
 		plt.axis([0, 100, 0, 100])
 		plt.xlabel('Current Score')
 		plt.ylabel('Effective Average')
 		plt.draw()
-		time.sleep(0.5)
+		#time.sleep(0.5)
 	plt.ioff()
 	plt.show()
 
