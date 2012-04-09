22dc18d93fc3ea7a4361073db565f61a050bddd8
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-09 12:06:31 -0700

    Implemented fromPrior(), make plots of it for testing

--- HazardModel.py
+++ HazardModel.py
@@ -18,9 +18,17 @@ class HazardModel:
 		self.mu0, self.mu1, self.L = mu0, mu1, L
 		self.compute()
 
-#	def fromPrior(self):
-#		self.mu0 = 
-
+	def fromPrior(self):
+		"""
+		Draw the parameters from the prior. Currently using
+		an ignorant prior.
+		"""
+		self.mu0 = np.exp(np.log(1.0) + np.log(1E2)*rng.rand())
+		self.mu1 = np.exp(np.log(1.0) + np.log(1E2)*rng.rand())
+		if self.mu1 < self.mu0:
+			self.mu0, self.mu1 = self.mu1, self.mu0
+		self.L = np.exp(np.log(0.1) + np.log(1E3)*rng.rand())
+		self.compute()
 
 	def compute(self):
 		"""
@@ -38,6 +46,9 @@ class HazardModel:
 		# Numerically compute actual average
 		# Note: can be underestimated due to range of HazardModel.x!
 		self.average = np.sum(HazardModel.x*np.exp(self.logf))
+
+	def __str__(self):
+		return str(self.mu0) + " " + str(self.mu1) + " " + str(self.L)
 		
 
 if __name__ == '__main__':
@@ -45,10 +56,21 @@ if __name__ == '__main__':
 	A simple main for testing
 	"""
 	import matplotlib.pyplot as plt
+	import time
 
 	m = HazardModel()
 
-	print(m.average)
-	plt.plot(HazardModel.x, np.exp(m.logG))
+	plt.ion()
+	plt.hold(False)
+	for i in xrange(0, 100):
+		m.fromPrior()
+		plt.plot(HazardModel.x, m.mu)
+		plt.title("Prior over a player's effective average curve")
+		plt.axis([0, 100, 0, 100])
+		plt.xlabel('Current Score')
+		plt.ylabel('Effective Average')
+		plt.draw()
+		time.sleep(0.5)
+	plt.ioff()
 	plt.show()
 
